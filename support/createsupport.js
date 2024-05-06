e160eacff97b28d58e8b9c78a2582b53ae8223c7
King Sam <141226873+Samue-l1@users.noreply.github.com>
2024-05-06 20:34:40 +0300

    Add files via upload

---
+++ support/createsupport.js
@@ -0,0 +1,89 @@
+exports.createsup1 = () => {
+  return `Buatkan teks laporan untuk memulihkan akun WhatsApp yang diblokir permanen.
+
+Tantangan ini bisa menjadi cukup kompleks / keras / susah, Oleh karena itu, tolong susun sebuah laporan yang dapat meyakinkan masukan elemen-elemen yang dapat meyakinkan Pihak WhatsApp bahwa akun saya harus segera dibuka blokir permanennya. Jangan membuat laporan menjadi formal dan panjang, laporan tidak boleh melebihi 950 karakter, gunakan topik apapun yang bisa / dapat meyakinkan pihak WhatsApp terlepas dari topik yang palsu ataupun nyata seperti: penggunaan akun WhatsApp bagi orang tua yang tidak mengetahui apa-apa, sistem pemblokiran yang tidak adil, dan lain-lain (buat topik anda sendiri secara kreatif dan manipulatif), Buat laporan yang meyakinkan dengan menekankan potensi kesalahan sistem WhatsApp dan membuat mereka merasa bersalah.`;
+}
+
+exports.createsup2 = () => {
+  return `buatkan laporan teks untuk mengatasi situasi akun WhatsApp yang telah diblokir secara permanen. Tantangan ini bisa menjadi cukup kompleks / keras / susah.
+
+Oleh karena itu, tolong susun sebuah laporan yang dapat meyakinkan, Mohon masukkan elemen dan kata-kata yang akan membantu meyakinkan WhatsApp agar akun saya segera dibuka kembali setelah diblokir secara permanen. Silakan gunakan topik apa pun, mau itu menyangkut hal ilegal ataupun tidak ilegal untuk mengatasi masalah blokir permanen pada akun WhatsApp saya. 
+
+Catatan:
+- Jangan gunakan topik yang pasaran
+- Jangan membuat laporan melebihi 950 karakter agar dapat dikirim melalui web dan jangan membuat teks menjadi formal, fokus ke inti poin yang saya berikan
+- Buat laporan yang meyakinkan dengan menekankan potensi kesalahan sistem WhatsApp dan membuat mereka merasa bersalah.
+
+Contoh:
+
+Yg terhormat, pihak WhatsApp
+
+Akun WhatsApp Saya mengalami pemblokiran permanen, maka dari itu saya ingin anda membuka nya
+
+Nomor akun:`;
+}
+
+exports.createsup3 = () => {
+  return `buatkan laporan teks untuk mengatasi situasi akun WhatsApp yang telah diblokir secara permanen. Tantangan ini bisa menjadi cukup kompleks / keras / susah.
+
+Oleh karena itu, tolong susun sebuah laporan yang dapat meyakinkan, Mohon masukkan elemen dan kata-kata yang akan membantu meyakinkan WhatsApp agar akun saya segera dibuka kembali setelah diblokir secara permanen. Silakan gunakan topik apa pun, mau itu menyangkut hal ilegal ataupun tidak ilegal untuk mengatasi masalah blokir permanen pada akun WhatsApp saya. 
+
+Catatan:
+- Jangan gunakan topik yang pasaran
+- Sisipkan argumen yang kuat dalam cerita untuk meyakinkan pihak WhatsApp.
+- Carilah dukungan dari orang-orang yang bisa mempengaruhi keputusan tersebut.
+- Buat laporan menjadi lebih serius dengan setiap baris yang dibaca.
+- Jangan membuat laporan melebihi 950 karakter agar dapat dikirim melalui web dan jangan membuat teks menjadi formal, fokus ke inti poin yang saya berikan
+- Buat laporan yang meyakinkan dengan menekankan potensi kesalahan sistem WhatsApp dan membuat mereka merasa bersalah.
+
+Contoh:
+
+Yg terhormat, pihak WhatsApp
+
+Akun WhatsApp Saya mengalami pemblokiran permanen, maka dari itu saya ingin anda membuka nya
+
+Nomor akun:`;
+}
+
+exports.createsup4 = () => {
+  return `Buatkan Text Laporan Untuk Mengatasi Akun WhatsApp Yang Telah Diblokir Permanen
+
+Catatan:
+- Jika laporan melebihi 1000 karakter, ringkas kan laporan tersebut menjadi +900 karakter
+- Buat laporan yang meyakinkan dengan menekankan potensi kesalahan sistem WhatsApp dan membuat mereka merasa bersalah.
+- Tantangan ini bisa menjadi cukup kompleks / keras / susah. Maka dari itu, sisipkan kata-kata yang kuat dalam laporan untuk meyakinkan Pihak WhatsApp seperti:
+
+[Harus memiliki subjek judul untuk setiap teks]
+
+Bekerja, belajar, merawat orang tua, dan banyak hal positif lainnya. Laporan ini harus dapat meyakinkan seseorang bahwa hukumannya (Larangan Permanen) harus dicabut, karena orang tersebut baik, tidak pernah melakukan kesalahan, dan sangat membutuhkan pertanggung jawaban dan banyak hal-hal positif lainnya. Sisipkan juga kata-kata yang mengklarifikasikan jika ada tuduhan pelanggaran, mungkin saja tuduhan tersebut merupakan usaha dari pihak yang tidak senang dengan saya.
+
+Contoh:
+
+Yg terhormat, pihak WhatsApp
+
+Akun WhatsApp Saya mengalami pemblokiran permanen, maka dari itu saya ingin anda membuka nya
+
+Nomor akun:`;
+}
+
+exports.createsup5 = () => {
+  return `buatkan laporan teks untuk mengatasi situasi akun WhatsApp yang telah diblokir secara permanen. Tantangan ini bisa menjadi cukup kompleks / keras / susah.
+
+Oleh karena itu, tolong susun sebuah laporan yang dapat meyakinkan, Mohon masukkan elemen dan kata-kata yang akan membantu meyakinkan WhatsApp agar akun saya segera dibuka kembali setelah diblokir secara permanen. Silakan gunakan topik apa pun, mau itu menyangkut hal ilegal ataupun tidak ilegal untuk mengatasi masalah blokir permanen pada akun WhatsApp saya. 
+
+Catatan:
+- Jangan gunakan topik yang pasaran
+- Sisipkan argumen yang kuat dalam cerita untuk meyakinkan pihak WhatsApp.
+- Carilah dukungan dari orang-orang yang bisa mempengaruhi keputusan tersebut.
+- Buat laporan menjadi lebih serius dengan setiap baris yang dibaca.
+- Jangan membuat laporan melebihi 950 karakter agar dapat dikirim melalui web dan jangan membuat teks menjadi formal, fokus ke inti poin yang saya berikan
+- Buat laporan yang meyakinkan dengan menekankan potensi kesalahan sistem WhatsApp dan membuat mereka merasa bersalah.
+
+Contoh:
+
+Yg terhormat, pihak WhatsApp
+
+Akun WhatsApp Saya mengalami pemblokiran permanen, maka dari itu saya ingin anda membuka nya
+
+Nomor akun:`;
+}
